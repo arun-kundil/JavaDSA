2c969f7310f292df18fa35956013896ea666c005
arun-kundil <arunkkd21@gmail.com>
2023-06-19 22:15:27 +0530

    HourGlass Sum solution

---
+++ src/com/dsa/learn/HourGlassSum.java
@@ -0,0 +1,45 @@
+
+//Problem - https://www.hackerrank.com/challenges/java-2d-array/problem
+package com.dsa.learn;
+
+import static java.util.stream.Collectors.toList;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class HourGlassSum {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+		List<List<Integer>> arr = new ArrayList<>();
+
+		IntStream.range(0, 6).forEach(i -> {
+			try {
+				arr.add(Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" ")).map(Integer::parseInt)
+						.collect(toList()));
+			} catch (IOException ex) {
+				throw new RuntimeException(ex);
+			}
+		});
+
+		bufferedReader.close();
+		List<Integer> sums = new ArrayList<>();
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				sums.add(arr.get(i).get(j) + arr.get(i).get(j + 1) + arr.get(i).get(j + 2) + arr.get(i + 1).get(j + 1)
+						+ arr.get(i + 2).get(j) + arr.get(i + 2).get(j + 1) + arr.get(i + 2).get(j + 2));
+			}
+
+		}
+
+		// System.out.println(sums);
+
+		System.out.println(Collections.max(sums));
+	}
+}
